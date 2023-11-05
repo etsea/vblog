1af61944f20d543ee18565e2097018c85d318908
Jeff Vos <jeff.vos@icloud.com>
2023-11-05 17:26:37 -0500

    changed default to serve on 127.0.0.1:8080

--- src/main.v
+++ src/main.v
@@ -46,7 +46,7 @@ fn add_func(cmd Command) ! {
 
 fn serve_func(cmd Command) ! {
 	mut server := Server{
-		addr: ':80'
+		addr: '127.0.0.1:8080'
 		handler: BlogHandler{}
 	}
 	server.listen_and_serve()
