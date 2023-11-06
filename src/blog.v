d3f2bb8e2963bd48b4c323b1a535d010e545d833
Jeff Vos <jeff.vos@icloud.com>
2023-11-05 22:58:20 -0500

    renamed most article references to 'post'

--- src/blog.v
+++ src/blog.v
@@ -27,7 +27,7 @@ fn (h BlogHandler) handle(req Request) Response {
 			generate_home_page(file_data)
 		}
 		.allposts {
-			generate_all_posts(file_data)
+			generate_all_posts_page(file_data)
 		}
 		.postpage {
 			generate_post_page(file_data, post_id, req.host)
@@ -47,7 +47,7 @@ fn (h BlogHandler) handle(req Request) Response {
 
 fn check_if_in_db(id int) bool {
 	posts_db := dbase.connect() or { panic(err) }
-	exists := posts_db.exec('select exists(select 1 from articles where id = ${id})') or { panic(err) }
+	exists := posts_db.exec('select exists(select 1 from posts where id = ${id})') or { panic(err) }
 	return if exists[0].vals[0] == '1' { true } else { false }
 }
 
@@ -56,9 +56,9 @@ pub fn generate_home_page(file_data static_data.FileData) string {
 	content = content.replace('@BLOGTITLE', blog_title).replace('@PAGETITLE', file_data.title)
 
 	posts_db := dbase.connect() or { panic(err) }
-	mut articles := posts_db.exec("select title, time_date, desc, author, id from articles order by id desc limit 10;") or { panic(err) }
-	mut articles_body := ''
-	for article in articles {
+	mut posts := posts_db.exec("select title, time_date, desc, author, id from posts order by id desc limit 10;") or { panic(err) }
+	mut posts_body := ''
+	for article in posts {
 		article_time := time.parse(article.vals[1]) or { panic(err) }
 		formatted_time := article_time.custom_format('h:mm A // MMM D YYYY')
 		article_title := article.vals[0]
@@ -66,33 +66,33 @@ pub fn generate_home_page(file_data static_data.FileData) string {
 		article_content := hlp.shorten_post(article.vals[2])
 		post_id := article.vals[4]
 
-		articles_body += ' '.repeat(8)
-		articles_body += '<article>\n'
-		articles_body += ' '.repeat(12)
-		articles_body += '<h2><a class="title" href="/post${post_id}">${article_title}</a></h2>\n'
-		articles_body += ' '.repeat(12)
-		articles_body += '<p class="date">${formatted_time} <span class="author">posted by ${article_author}</span></p>\n'
-		articles_body += ' '.repeat(12)
-		articles_body += '<p>${article_content}</p>\n'
-		articles_body += ' '.repeat(12)
-		articles_body += '<img src="avatar.bmp" alt="Author Avatar" class="avatar">\n'
-		articles_body += ' '.repeat(8)
-		articles_body += '</article>\n'
+		posts_body += ' '.repeat(8)
+		posts_body += '<article>\n'
+		posts_body += ' '.repeat(12)
+		posts_body += '<h2><a class="title" href="/post${post_id}">${article_title}</a></h2>\n'
+		posts_body += ' '.repeat(12)
+		posts_body += '<p class="date">${formatted_time} <span class="author">posted by ${article_author}</span></p>\n'
+		posts_body += ' '.repeat(12)
+		posts_body += '<p>${article_content}</p>\n'
+		posts_body += ' '.repeat(12)
+		posts_body += '<img src="avatar.bmp" alt="Author Avatar" class="avatar">\n'
+		posts_body += ' '.repeat(8)
+		posts_body += '</article>\n'
 	}
 
-	content = content.replace('@POSTS', articles_body)
+	content = content.replace('@POSTS', posts_body)
 
 	return content
 }
 
-pub fn generate_all_posts(file_data static_data.FileData) string {
+pub fn generate_all_posts_page(file_data static_data.FileData) string {
 	mut content := file_data.content
 	content = content.replace('@BLOGTITLE', blog_title).replace('@PAGETITLE', file_data.title)
 
 	posts_db := dbase.connect() or { panic(err) }
-	mut articles := posts_db.exec("select title, time_date, desc, author, id from articles order by id desc;") or { panic(err) }
-	mut articles_body := ''
-	for article in articles {
+	mut posts := posts_db.exec("select title, time_date, desc, author, id from articles order by id desc;") or { panic(err) }
+	mut posts_body := ''
+	for article in posts {
 		article_time := time.parse(article.vals[1]) or { panic(err) }
 		formatted_time := article_time.custom_format('h:mm A // MMM D YYYY')
 		article_title := article.vals[0]
@@ -100,21 +100,21 @@ pub fn generate_all_posts(file_data static_data.FileData) string {
 		article_content := article.vals[2]
 		post_id := article.vals[4]
 
-		articles_body += ' '.repeat(8)
-		articles_body += '<article>\n'
-		articles_body += ' '.repeat(12)
-		articles_body += '<h2><a class="title" href="/post${post_id}">${article_title}</a></h2>\n'
-		articles_body += ' '.repeat(12)
-		articles_body += '<p class="date">${formatted_time} <span class="author">posted by ${article_author}</span></p>\n'
-		articles_body += ' '.repeat(12)
-		articles_body += '<p>${article_content}</p>\n'
-		articles_body += ' '.repeat(12)
-		articles_body += '<img src="avatar.bmp" alt="Author Avatar" class="avatar">\n'
-		articles_body += ' '.repeat(8)
-		articles_body += '</article>\n'
+		posts_body += ' '.repeat(8)
+		posts_body += '<article>\n'
+		posts_body += ' '.repeat(12)
+		posts_body += '<h2><a class="title" href="/post${post_id}">${article_title}</a></h2>\n'
+		posts_body += ' '.repeat(12)
+		posts_body += '<p class="date">${formatted_time} <span class="author">posted by ${article_author}</span></p>\n'
+		posts_body += ' '.repeat(12)
+		posts_body += '<p>${article_content}</p>\n'
+		posts_body += ' '.repeat(12)
+		posts_body += '<img src="avatar.bmp" alt="Author Avatar" class="avatar">\n'
+		posts_body += ' '.repeat(8)
+		posts_body += '</article>\n'
 	}
 
-	content = content.replace('@POSTS', articles_body)
+	content = content.replace('@POSTS', posts_body)
 
 	return content
 }
@@ -126,33 +126,33 @@ pub fn generate_post_page(file_data static_data.FileData, post_id int, base_url 
 	content = content.replace('@POSTNUMBER', post_id.str())
 
 	posts_db := dbase.connect() or { panic(err) }
-	mut articles := posts_db.exec("select title, time_date, content, author from articles where id = ${post_id};") or { panic(err) }
-	mut articles_body := ''
-	for article in articles {
+	mut posts := posts_db.exec("select title, time_date, content, author from articles where id = ${post_id};") or { panic(err) }
+	mut posts_body := ''
+	for article in posts {
 		article_time := time.parse(article.vals[1]) or { panic(err) }
 		formatted_time := article_time.custom_format('h:mm A // MMM D YYYY')
 		article_title := article.vals[0]
 		article_author := article.vals[3]
 		article_content := article.vals[2]
 
-		articles_body += ' '.repeat(8)
-		articles_body += '<article>\n'
-		articles_body += ' '.repeat(12)
-		articles_body += '<h2>${article_title}</h2>\n'
-		articles_body += ' '.repeat(12)
-		articles_body += '<p class="date">${formatted_time} <span class="author">posted by ${article_author}</span></p>\n'
-		articles_body += ' '.repeat(12)
-		articles_body += '<p>${article_content}</p>\n'
-		articles_body += ' '.repeat(12)
-		articles_body += '<img src="avatar.bmp" alt="Author Avatar" class="avatar">\n'
-		articles_body += ' '.repeat(8)
-		articles_body += '</article>\n'
+		posts_body += ' '.repeat(8)
+		posts_body += '<article>\n'
+		posts_body += ' '.repeat(12)
+		posts_body += '<h2>${article_title}</h2>\n'
+		posts_body += ' '.repeat(12)
+		posts_body += '<p class="date">${formatted_time} <span class="author">posted by ${article_author}</span></p>\n'
+		posts_body += ' '.repeat(12)
+		posts_body += '<p>${article_content}</p>\n'
+		posts_body += ' '.repeat(12)
+		posts_body += '<img src="avatar.bmp" alt="Author Avatar" class="avatar">\n'
+		posts_body += ' '.repeat(8)
+		posts_body += '</article>\n'
 		fmt_title, fmt_content := article_title.replace('"', '&quot;'), hlp.shorten_post(article_content.replace('"', '&quot;'))
 		content = content.replace('@POSTNAME', fmt_title)
 		content = content.replace('@BAREPOSTCONTENT', fmt_content)
 	}
 
-	content = content.replace('@POSTCONTENT', articles_body)
+	content = content.replace('@POSTCONTENT', posts_body)
 
 	return content
 }
